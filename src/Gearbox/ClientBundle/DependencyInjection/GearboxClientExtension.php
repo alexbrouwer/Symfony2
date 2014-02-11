b89f84f2338681da549d0d6b1b7d937659b944fa
Alexander Brouwer <alexander@odmedia.nl>
2014-02-11 16:39:25 +0100

    Playing around with scss and assets rewriting

--- src/Gearbox/ClientBundle/DependencyInjection/GearboxClientExtension.php
+++ src/Gearbox/ClientBundle/DependencyInjection/GearboxClientExtension.php
@@ -4,6 +4,7 @@ namespace Gearbox\ClientBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -12,7 +13,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class GearboxClientExtension extends Extension
+class GearboxClientExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +25,39 @@ class GearboxClientExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        if (true === isset($bundles['AsseticBundle'])) {
+            $this->configureAsseticBundle($container, $config);
+        }
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param array $config
+     */
+    protected function configureAsseticBundle(ContainerBuilder $container, array $config)
+    {
+        foreach (array_keys($container->getExtensions()) as $name) {
+            switch ($name) {
+                case 'assetic':
+                    $container->prependExtensionConfig(
+                        $name,
+                        array('bundles' => array('GearboxClientBundle'))
+                    );
+                    break;
+            }
+        }
     }
 }
